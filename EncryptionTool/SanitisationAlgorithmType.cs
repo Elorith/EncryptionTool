72e1ec8a09877dacfcec3d1e7d4ab448a90b8b1b
Elorith <elorith@protonmail.com>
2021-12-30 19:51:13 +0000

    Modify sanitisation algorithm type descriptions

--- EncryptionTool/SanitisationAlgorithmType.cs
+++ EncryptionTool/SanitisationAlgorithmType.cs
@@ -1,14 +1,14 @@
 public enum SanitisationAlgorithmType : int
 {
     /// <summary>
-    /// Implements the US DoD 5220.22-M (E) data sanitisation algorithm. Overwrites files 3 times. This method offers "medium" security. Use it only
-    /// to erase files that do not contain highly sensitive information.
+    /// Implements the US DoD 5220.22-M (E) data sanitisation algorithm. Overwrites files 3 times. This method offers "high" security but use it only
+    /// to erase files that do not contain the very highest level of sensitive information.
     /// </summary>
     DoDFast = 1,
     
     /// <summary>
-    /// Implements the US DoD 5220.22-M (ECE) data sanitisation algorithm. Overwrites files 7 times. This method is considered highly secure and can
-    /// be used to erase files that contain highly sensitive information.
+    /// Implements the US DoD 5220.22-M (ECE) data sanitisation algorithm. Overwrites files 7 times. This method is considered extremely secure and
+    /// can be used to erase files that contain the very highest level of sensitive information.
     /// </summary>
     DoDSensitive = 2
 }
