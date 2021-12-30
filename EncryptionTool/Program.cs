741017b41783d273f55f051b7558d98925d2a4ca
Elorith <elorith@protonmail.com>
2021-12-30 03:53:01 +0000

    .

--- EncryptionTool/Program.cs
+++ EncryptionTool/Program.cs
@@ -5,6 +5,12 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        /*
+         * 1) Take file and encrypt using the user's personal key (which needs to be entered each time a security related action is taken).
+         * 2) Write encrypted variation of the file to disk.
+         * 3) Verify that the newly written file is valid.
+         * 4) Securely erase the original file from disk using implementation of US DoD 5220.22-M (ECE).
+         */
         while (true)
         {
             string path = Console.ReadLine();
