32a54816bbca5067b6f1421b9067404c8f6404e2
Elorith <elorith@protonmail.com>
2021-12-31 04:44:30 +0000

    Add Decrypt function

--- EncryptionTool/CryptographyProvider.cs
+++ EncryptionTool/CryptographyProvider.cs
@@ -6,39 +6,78 @@ public class CryptographyProvider
 {
      private void Encrypt(string path, string password)
      {
-          byte[] salt = new byte[32];
-          using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
-          {
-               rng.GetBytes(salt);
-          }
-
           using (RijndaelManaged aes = new RijndaelManaged())
           {
                aes.Padding = PaddingMode.ISO10126;
                aes.Mode = CipherMode.CBC;
-               
-               using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt))
-               {
-                    aes.Key = key.GetBytes(32);
-                    aes.GenerateIV();    
-               }
 
                using (FileStream output = new FileStream(path + ".aes", FileMode.Create))
                {
-                    output.Write(salt, 0, salt.Length);
-                    output.Write(aes.IV, 0, aes.IV.Length);
-               
+                    byte[] salt = new byte[32];
+                    byte[] iv = new byte[32];
+
+                    using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+                    {
+                         rng.GetBytes(salt);
+                         rng.GetBytes(iv);
+                    }
+                    output.Write(salt, 0, 32);
+                    output.Write(iv, 0, 32);
+                    
+                    using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt))
+                    {
+                         aes.Key = key.GetBytes(32);
+                         aes.IV = iv;
+                    }
+
                     using (CryptoStream stream = new CryptoStream(output, aes.CreateEncryptor(), CryptoStreamMode.Write))
                     {
                          using (FileStream input = new FileStream(path, FileMode.Open))
                          {
                               byte[] buffer = new byte[1048576];
-                              int read;
-                              while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                              int bytesRead;
+                              while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
                               {
-                                   stream.Write(buffer, 0, read);
+                                   stream.Write(buffer, 0, bytesRead);
                               }
                          }
+                    }
+               }
+          }
+     }
+
+     private void Decrypt(string path, string password)
+     {
+          using (RijndaelManaged aes = new RijndaelManaged())
+          {
+               aes.Padding = PaddingMode.ISO10126;
+               aes.Mode = CipherMode.CBC;
+               
+               using (FileStream input = new FileStream(path, FileMode.Open))
+               {
+                    byte[] salt = new byte[32];
+                    byte[] iv = new byte[32];
+
+                    input.Read(salt, 0, 32);
+                    input.Read(iv, 0, 32);
+                    
+                    using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt))
+                    {
+                         aes.Key = key.GetBytes(32);
+                         aes.IV = iv;
+                    }
+
+                    using (CryptoStream stream = new CryptoStream(input, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                         using (FileStream output = new FileStream(Path.GetFileNameWithoutExtension(path), FileMode.Create))
+                         {
+                              byte[] buffer = new byte[1048576];
+                              int bytesRead;
+                              while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                              {
+                                   output.Write(buffer, 0, bytesRead);
+                              }   
+                         }    
                     }
                }
           }
