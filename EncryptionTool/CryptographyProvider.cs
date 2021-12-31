7476de6cf5daa2947e8edb98484c157d90976554
Elorith <elorith@protonmail.com>
2021-12-31 16:45:51 +0000

    Create seperate functions for cryptography stream write/read

--- EncryptionTool/CryptographyProvider.cs
+++ EncryptionTool/CryptographyProvider.cs
@@ -1,10 +1,10 @@
-using System;
 using System.IO;
 using System.Security.Cryptography;
-using System.Text;
 
 public class CryptographyProvider
 {
+     private const ulong encryptionBufferSize = 1048576;
+     
      public void Encrypt(string path, string password)
      {
           using (RijndaelManaged aes = new RijndaelManaged())
@@ -12,37 +12,29 @@ public class CryptographyProvider
                aes.Padding = PaddingMode.ISO10126;
                aes.Mode = CipherMode.CBC;
 
-               string outputPath = path + ".aes";
-               using (FileStream output = new FileStream(outputPath, FileMode.Create))
+               using (FileStream input = new FileStream(path, FileMode.Open))
                {
-                    byte[] salt = new byte[32];
-                    byte[] iv = new byte[16];
+                    string outputPath = path + ".aes";
+                    using (FileStream output = new FileStream(outputPath, FileMode.Create))
+                    {
+                         byte[] salt = new byte[32];
+                         byte[] iv = new byte[16];
 
-                    using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
-                    {
-                         rng.GetBytes(salt);
-                         rng.GetBytes(iv);
-                    }
-                    output.Write(salt, 0, 32);
-                    output.Write(iv, 0, 16);
-                    
-                    using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt))
-                    {
-                         aes.Key = key.GetBytes(32);
-                         aes.IV = iv;
-                    }
-
-                    using (CryptoStream stream = new CryptoStream(output, aes.CreateEncryptor(), CryptoStreamMode.Write))
-                    {
-                         using (FileStream input = new FileStream(path, FileMode.Open))
+                         using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
                          {
-                              byte[] buffer = new byte[1048576];
-                              int bytesRead;
-                              while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
-                              {
-                                   stream.Write(buffer, 0, bytesRead);
-                              }
+                              rng.GetBytes(salt);
+                              rng.GetBytes(iv);
                          }
+                         output.Write(salt, 0, 32);
+                         output.Write(iv, 0, 16);
+                    
+                         using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt))
+                         {
+                              aes.Key = key.GetBytes(32);
+                              aes.IV = iv;
+                         }
+                         
+                         this.Encrypt(input, output, aes.CreateEncryptor());
                     }
                }
           }
@@ -59,41 +51,59 @@ public class CryptographyProvider
                
                using (FileStream input = new FileStream(path, FileMode.Open))
                {
-                    byte[] salt = new byte[32];
-                    byte[] iv = new byte[16];
+                    string outputPath = Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path));
+                    using (FileStream output = new FileStream(outputPath, FileMode.Create))
+                    {
+                         byte[] salt = new byte[32];
+                         byte[] iv = new byte[16];
 
-                    input.Read(salt, 0, 32);
-                    input.Read(iv, 0, 16);
+                         input.Read(salt, 0, 32);
+                         input.Read(iv, 0, 16);
                     
-                    using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt))
-                    {
-                         aes.Key = key.GetBytes(32);
-                         aes.IV = iv;
-                    }
-
-                    using (CryptoStream stream = new CryptoStream(input, aes.CreateDecryptor(), CryptoStreamMode.Read))
-                    {
-                         string outputPath = Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path));
-                         using (FileStream output = new FileStream(outputPath, FileMode.Create))
+                         using (Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt))
                          {
-                              byte[] buffer = new byte[1048576];
-                              try
-                              {
-                                   int bytesRead;
-                                   while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-                                   {
-                                        output.Write(buffer, 0, bytesRead);
-                                   }   
-                              }
-                              catch (CryptographicException ex)
-                              {
-                                   Logger.Singleton.WriteLine("'" + path + " could not be decrypted.");
-                              }
-                         }    
+                              aes.Key = key.GetBytes(32);
+                              aes.IV = iv;
+                         }
+                         
+                         this.Decrypt(input, output, aes.CreateDecryptor());
                     }
                }
           }
           
           Logger.Singleton.WriteLine("'" + path + " has been successfully decrypted.");
+     }
+
+     private void Encrypt(Stream input, Stream output, ICryptoTransform encryptor)
+     {
+          using (CryptoStream stream = new CryptoStream(output, encryptor, CryptoStreamMode.Write))
+          {
+               byte[] buffer = new byte[CryptographyProvider.encryptionBufferSize];
+               int bytesRead;
+               while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+               {
+                    stream.Write(buffer, 0, bytesRead);
+               }
+          }
+     }
+
+     private void Decrypt(Stream input, Stream output, ICryptoTransform decryptor)
+     {
+          using (CryptoStream stream = new CryptoStream(input, decryptor, CryptoStreamMode.Read))
+          {
+               byte[] buffer = new byte[CryptographyProvider.encryptionBufferSize];
+               try
+               {
+                    int bytesRead;
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                         output.Write(buffer, 0, bytesRead);
+                    }   
+               }
+               catch (CryptographicException ex)
+               {
+                    Logger.Singleton.WriteLine("Stream could not be decrypted.");
+               }
+          }
      }
 }
