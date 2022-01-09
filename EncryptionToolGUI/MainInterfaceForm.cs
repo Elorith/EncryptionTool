ade2ecdf52e5cb3dc01216cdf88ffe090b97eb23
Elorith <elorith@protonmail.com>
2022-01-09 04:43:55 +0000

    Split into two projects for CLI and GUI

---
+++ EncryptionToolGUI/MainInterfaceForm.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+public partial class MainInterfaceForm : Form
+{
+    public MainInterfaceForm()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ EncryptionToolGUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+public class Program
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    public static void Main() => new Program().RunGraphicalUserInterface();
+
+    // GUI implementation of the encryption tool.
+    public void RunGraphicalUserInterface()
+    {
+        MainInterfaceForm form = new MainInterfaceForm();
+        
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(form);
+    }
+}
