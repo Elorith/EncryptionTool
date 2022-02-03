ba9605e8ba6663bc9b74d0a7e5591e8a7f47bce9
Elorith <elorith@protonmail.com>
2022-02-03 03:00:43 +0000

    Select encryption mode

--- EncryptionToolGUI/FormMainInterface.Designer.cs
+++ EncryptionToolGUI/FormMainInterface.Designer.cs
@@ -27,25 +27,79 @@
     /// </summary>
     private void InitializeComponent()
     {
-        this.toolStripDropDownMenu1 = new System.Windows.Forms.ToolStripDropDownMenu();
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainInterface));
+        this.TextBoxSelectedPath = new System.Windows.Forms.RichTextBox();
+        this.ButtonSelectPath = new System.Windows.Forms.Button();
+        this.DialogSelectPathFile = new System.Windows.Forms.OpenFileDialog();
+        this.CheckBoxEncryptFiles = new System.Windows.Forms.CheckBox();
+        this.CheckBoxEncryptDirectories = new System.Windows.Forms.CheckBox();
+        this.DialogSelectPathDirectory = new System.Windows.Forms.FolderBrowserDialog();
         this.SuspendLayout();
         // 
-        // toolStripDropDownMenu1
+        // TextBoxSelectedPath
         // 
-        this.toolStripDropDownMenu1.Name = "toolStripDropDownMenu1";
-        this.toolStripDropDownMenu1.Size = new System.Drawing.Size(61, 4);
+        this.TextBoxSelectedPath.Location = new System.Drawing.Point(12, 15);
+        this.TextBoxSelectedPath.Name = "TextBoxSelectedPath";
+        this.TextBoxSelectedPath.Size = new System.Drawing.Size(576, 22);
+        this.TextBoxSelectedPath.TabIndex = 0;
+        this.TextBoxSelectedPath.Text = "";
+        // 
+        // ButtonSelectPath
+        // 
+        this.ButtonSelectPath.Location = new System.Drawing.Point(594, 15);
+        this.ButtonSelectPath.Name = "ButtonSelectPath";
+        this.ButtonSelectPath.Size = new System.Drawing.Size(71, 22);
+        this.ButtonSelectPath.TabIndex = 1;
+        this.ButtonSelectPath.Text = "Browse";
+        this.ButtonSelectPath.UseVisualStyleBackColor = true;
+        this.ButtonSelectPath.Click += new System.EventHandler(this.ButtonSelectPath_Click);
+        // 
+        // CheckBoxEncryptFiles
+        // 
+        this.CheckBoxEncryptFiles.Checked = true;
+        this.CheckBoxEncryptFiles.CheckState = System.Windows.Forms.CheckState.Checked;
+        this.CheckBoxEncryptFiles.Location = new System.Drawing.Point(12, 45);
+        this.CheckBoxEncryptFiles.Name = "CheckBoxEncryptFiles";
+        this.CheckBoxEncryptFiles.Size = new System.Drawing.Size(98, 25);
+        this.CheckBoxEncryptFiles.TabIndex = 2;
+        this.CheckBoxEncryptFiles.Text = "File";
+        this.CheckBoxEncryptFiles.UseVisualStyleBackColor = true;
+        this.CheckBoxEncryptFiles.CheckedChanged += new System.EventHandler(this.CheckBoxEncryptFiles_CheckedChanged);
+        // 
+        // CheckBoxEncryptDirectories
+        // 
+        this.CheckBoxEncryptDirectories.Location = new System.Drawing.Point(129, 45);
+        this.CheckBoxEncryptDirectories.Name = "CheckBoxEncryptDirectories";
+        this.CheckBoxEncryptDirectories.Size = new System.Drawing.Size(109, 25);
+        this.CheckBoxEncryptDirectories.TabIndex = 3;
+        this.CheckBoxEncryptDirectories.Text = "Directory";
+        this.CheckBoxEncryptDirectories.UseVisualStyleBackColor = true;
+        this.CheckBoxEncryptDirectories.CheckedChanged += new System.EventHandler(this.CheckBoxEncryptDirectories_CheckedChanged);
         // 
         // FormMainInterface
         // 
         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.ClientSize = new System.Drawing.Size(672, 302);
+        this.Controls.Add(this.CheckBoxEncryptDirectories);
+        this.Controls.Add(this.CheckBoxEncryptFiles);
+        this.Controls.Add(this.ButtonSelectPath);
+        this.Controls.Add(this.TextBoxSelectedPath);
+        this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
         this.Name = "FormMainInterface";
-        this.Text = "Form1";
+        this.Text = "Encryption Tool";
         this.ResumeLayout(false);
     }
 
-    private System.Windows.Forms.ToolStripDropDownMenu toolStripDropDownMenu1;
+    private System.Windows.Forms.FolderBrowserDialog DialogSelectPathDirectory;
+
+    private System.Windows.Forms.CheckBox CheckBoxEncryptFiles;
+    private System.Windows.Forms.CheckBox CheckBoxEncryptDirectories;
+
+    private System.Windows.Forms.OpenFileDialog DialogSelectPathFile;
+
+    private System.Windows.Forms.RichTextBox TextBoxSelectedPath;
+    private System.Windows.Forms.Button ButtonSelectPath;
 
     #endregion
 }
--- EncryptionToolGUI/FormMainInterface.cs
+++ EncryptionToolGUI/FormMainInterface.cs
@@ -1,9 +1,89 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
 
 public partial class FormMainInterface : Form
 {
+    private bool checkboxChangedFlag = false;
+    
     public FormMainInterface()
     {
         this.InitializeComponent();
+    }
+
+    public bool HasFileEncryptionMode()
+    {
+        return this.CheckBoxEncryptFiles.Checked && !this.CheckBoxEncryptDirectories.Checked;
+    }
+    
+    public bool HasDirectoryEncryptionMode()
+    {
+        return this.CheckBoxEncryptDirectories.Checked && !this.CheckBoxEncryptFiles.Checked;
+    }
+
+    public string Browse()
+    {
+        string path;
+        if (this.HasFileEncryptionMode())
+        {
+            DialogResult result = this.DialogSelectPathFile.ShowDialog();
+            path = this.DialogSelectPathFile.FileName;
+            
+            if (result != DialogResult.OK || !File.Exists(path))
+            {
+                throw new Exception("Selected file path invalid");
+            }
+        }
+        else if (this.HasDirectoryEncryptionMode())
+        {
+            DialogResult result = this.DialogSelectPathDirectory.ShowDialog();
+            path = this.DialogSelectPathDirectory.SelectedPath;
+            
+            if (result != DialogResult.OK || !Directory.Exists(path))
+            {
+                throw new Exception("Selected directory path invalid");
+            }
+        }
+        else
+        {
+            throw new Exception("Neither encryption mode is selected");
+        }
+        
+        return path;
+    }
+
+    private void ButtonSelectPath_Click(object sender, EventArgs e)
+    {
+        this.TextBoxSelectedPath.Text = this.Browse();
+    } 
+
+    private void CheckBoxEncryptFiles_CheckedChanged(object sender, EventArgs e)
+    {
+        if (this.CheckBoxEncryptFiles.Checked)
+        {
+            this.checkboxChangedFlag = true;
+            this.CheckBoxEncryptDirectories.Checked = false;
+            this.checkboxChangedFlag = false;
+            this.TextBoxSelectedPath.Text = "";
+        }
+        else if (!checkboxChangedFlag)
+        {
+            this.CheckBoxEncryptFiles.Checked = true;
+        }
+    }
+
+    private void CheckBoxEncryptDirectories_CheckedChanged(object sender, EventArgs e)
+    {
+        if (this.CheckBoxEncryptDirectories.Checked)
+        {
+            this.checkboxChangedFlag = true;
+            this.CheckBoxEncryptFiles.Checked = false;
+            this.checkboxChangedFlag = false;
+            this.TextBoxSelectedPath.Text = "";
+        }
+        else if (!checkboxChangedFlag)
+        {
+            this.CheckBoxEncryptDirectories.Checked = true;
+        }
     }
 }
