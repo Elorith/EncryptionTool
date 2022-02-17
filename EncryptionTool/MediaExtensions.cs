d6c8a192e0209f0e5cb702948bdac85a26e17b82
Elorith <elorith@protonmail.com>
2022-02-17 20:59:12 +0000

    MediaExtensions.cs

---
+++ EncryptionTool/MediaExtensions.cs
@@ -0,0 +1,22 @@
+using System.IO;
+
+public static class MediaExtensions
+{
+    private static readonly string[] videoExtensionsList =
+    {
+        ".mp4", ".mov"
+    };
+
+    public static bool IsFileVideo(string path)
+    {
+        foreach (string extension in MediaExtensions.videoExtensionsList)
+        {
+            if (Path.HasExtension(extension))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
