3472d4fb040c5b12864201bf11132921e400ca06
Elorith <elorith@protonmail.com>
2022-02-26 00:16:47 +0000

    Store thumbnails

--- EncryptionTool/MediaExtensions.cs
+++ EncryptionTool/MediaExtensions.cs
@@ -9,9 +9,9 @@ public static class MediaExtensions
 {
     private const double mediaPreviewImagePosition = 0.5;
     
+    private const string ffprobeFileName = "ffprobe.exe";
     private const string ffmpegFileName = "ffmpeg.exe";
-    private const string ffmpegPathTempOutputFile = "tempThumb.jpg";
-    
+
     private static readonly string[] videoExtensionsList =
     {
         "mp4", "mov"
@@ -47,31 +47,60 @@ public static class MediaExtensions
 
     private static byte[] CreatePreviewImage(string path, double position)
     {
-        ProcessStartInfo info = new ProcessStartInfo();
-        info.FileName = Path.Combine(Environment.CurrentDirectory, MediaExtensions.ffmpegFileName);
-        info.Arguments = "-i " + path + " -ss 00:00:00.100 -vframes 1 " + MediaExtensions.ffmpegPathTempOutputFile;
-        info.CreateNoWindow = true;
-        info.UseShellExecute = false;
-        info.WorkingDirectory = Environment.CurrentDirectory;
-
-        using (Process process = new Process())
+        int lengthInMilliseconds = 0;
+        using (Process process1 = new Process())
         {
-            process.StartInfo = info;
+            ProcessStartInfo info1 = new ProcessStartInfo
+            {
+                FileName = Path.Combine(Environment.CurrentDirectory, MediaExtensions.ffprobeFileName),
+                Arguments = "-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 " + path/*+ " -sexagesimal"*/,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                WorkingDirectory = Environment.CurrentDirectory
+            };
 
-            process.Start();
-            process.WaitForExit();
-        }
+            process1.StartInfo = info1;
 
-        byte[] bytes;
-        using (MemoryStream stream = new MemoryStream())
-        {
-            Image image = Image.FromFile(Path.Combine(Environment.CurrentDirectory, MediaExtensions.ffmpegPathTempOutputFile));
+            process1.Start();
+            process1.WaitForExit();
             
-            image.Save(stream, ImageFormat.Jpeg);
-            bytes = stream.ToArray();
+            lengthInMilliseconds = (int)(Convert.ToDouble(process1.StandardOutput.ReadToEnd()) * 1000);
         }
         
-        File.Delete(MediaExtensions.ffmpegPathTempOutputFile);
+        double positionInMilliseconds = lengthInMilliseconds * position;
+        TimeSpan timespan = TimeSpan.FromMilliseconds(positionInMilliseconds);
+
+        string seek = timespan.Hours.ToString("00") + ":" + timespan.Minutes.ToString("00") + ":" + timespan.Seconds.ToString("00") + "." + timespan.Milliseconds.ToString("000");
+        
+        using (Process process2 = new Process())
+        {
+            ProcessStartInfo info2 = new ProcessStartInfo
+            {
+                FileName = Path.Combine(Environment.CurrentDirectory, MediaExtensions.ffmpegFileName),
+                Arguments = "-i " + path + " -ss " + seek + " -vframes 1 tempThumb.jpg",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                WorkingDirectory = Environment.CurrentDirectory
+            };
+
+            process2.StartInfo = info2;
+
+            process2.Start();
+            process2.WaitForExit();
+        }
+        
+        byte[] bytes = null;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            using (Image image = Image.FromFile(Path.Combine(Environment.CurrentDirectory, "tempThumb.jpg")))
+            {
+                image.Save(stream, ImageFormat.Jpeg);
+                bytes = stream.ToArray();
+            }
+        }
+        
+        File.Delete(Path.Combine(Environment.CurrentDirectory, "tempThumb.jpg"));
         
         return bytes;
     }
